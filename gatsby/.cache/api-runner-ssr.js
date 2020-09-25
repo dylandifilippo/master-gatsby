83e96c4e5c866a73056771f077c573e4f330deff
Dylan Di Filppo <dylan.di.filippo@gmail.com>
2020-09-25 10:22:10 +0200

    global styles

--- gatsby/.cache/api-runner-ssr.js
+++ gatsby/.cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('/Users/macbookpro/code/dylandifilippo/Wes Bos/master-gatsby-master/starter-files/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
