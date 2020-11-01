59f62ab08db03d03d3774bb12796f1f2f0d0729f
Dylan Di Filppo <dylan.di.filippo@gmail.com>
2020-11-01 15:26:38 +0100

    11 - Serverless Functions

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Nav from '../components/Nav';
-import Layout from '../components/Layout';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
 export default function Homepage() {
+  const { slicemasters, hotSlices } = useLatestData();
   return (
-    <>
-      <p>Hey! I'm the homepage!</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown</h1>
+      <p>Open 11a to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
